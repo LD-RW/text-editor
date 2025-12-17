19a1d3fb797064a1796830bd42d4356ac2611fc5
LD-RW <basheermazari4@gmail.com>
2025-12-17 17:47:26 +0300

    Added the disableRowMode function to retore original terminal flags

--- kilo.c
+++ kilo.c
@@ -1,14 +1,23 @@
+#include<stdlib.h>
 #include <unistd.h>
 #include<termios.h>
 
+struct termios orig_termious;
+
+void disableRowMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termious);
+}
+
 void enableRowMode(){
+    tcgetattr(STDIN_FILENO, &orig_termious);
+    atexit(disableRowMode);
+
     struct termios raw;
 
     tcgetattr(STDIN_FILENO, &raw);
-
     raw.c_lflag &= ~(ECHO);
-
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    
 }
 
 int main(){

574421421eb125d854657285ce5197dc1b767478
LD-RW <basheermazari4@gmail.com>
2025-12-20 19:26:48 +0300

    Turned on Raw Mode

--- kilo.c
+++ kilo.c
@@ -1,28 +1,54 @@
+// includes
 #include<stdlib.h>
 #include <unistd.h>
 #include<termios.h>
+#include<ctype.h>
+#include<stdio.h>
+#include<errno.h>
 
+// data
 struct termios orig_termious;
 
+// terminal
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
+
 void disableRowMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termious);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termious) == -1) die("tcsetattr");
 }
 
 void enableRowMode(){
-    tcgetattr(STDIN_FILENO, &orig_termious);
+    if(tcgetattr(STDIN_FILENO, &orig_termious) == -1) die("tcgetattr");
     atexit(disableRowMode);
 
-    struct termios raw;
+    struct termios raw = orig_termious;
 
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termious) == -1) die("tcsetattr");
     
 }
 
+// main
 int main(){
     enableRowMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while(true){
+        char c = '\0';
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if(iscntrl(c)){
+            printf("%d\r\n", c);
+        }
+        else{
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if(c == 'q') break;
+
+    }
     return 0;
 }

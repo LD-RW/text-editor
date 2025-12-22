e361118133ab669faf7c22b44e439d87b927ebc5
LD-RW <basheermazari4@gmail.com>
2025-12-22 18:20:20 +0300

    Add DEL_KEY support for cursor movement, but it doesn't do anything for now

--- kilo.c
+++ kilo.c
@@ -16,6 +16,7 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  DEL_KEY,
   HOME_KEY,
   END_KEY,
   PAGE_UP,
@@ -80,6 +81,7 @@ int editorReadKey(){
                 if(seq[2] == '~'){
                     switch(seq[1]){
                         case '1': return HOME_KEY;
+                        case '3': return DEL_KEY; // doesn't do anything for now
                         case '4': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;

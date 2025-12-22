34ea92468528827f48716850e94213273e95d6e5
LD-RW <basheermazari4@gmail.com>
2025-12-22 18:13:49 +0300

    Add support for page up and page down keys for cursor movement

--- kilo.c
+++ kilo.c
@@ -15,7 +15,9 @@ enum editorKey {
   ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -60,23 +62,37 @@ void enableRawMode(){
 int editorReadKey(){
     int nread;
     char c;
+    // Ignore the zeros generated because of the VTIME and VMIN
     while((nread = read(STDIN_FILENO, &c, 1)) != 1){
         if(nread == -1 && errno != EAGAIN) die("read");
     }
+    // Investigation mode, check if the input is a special press (escape character)
     if (c == '\x1b') {
         char seq[3];
         if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+        // If you press a regular escape key, this read will time out and return 0
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
         if (seq[0] == '[') {
-            switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+            if(seq[1] >= '0' && seq[1] <= '9'){
+                if(read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if(seq[2] == '~'){
+                    switch(seq[1]){
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            }
+            else{
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                }
             }
         }
         return '\x1b';
-  } else{
+  } else {
         return c;
     }
 }
@@ -168,6 +184,18 @@ void editorProcessKeypress(){
         write(STDOUT_FILENO, "\x1b[H", 3);
         exit(0);
         break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenRows;
+                while(times--){
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+                }   
+            }
+            break;
+
+
         case ARROW_UP:
         case ARROW_LEFT:
         case ARROW_DOWN:
